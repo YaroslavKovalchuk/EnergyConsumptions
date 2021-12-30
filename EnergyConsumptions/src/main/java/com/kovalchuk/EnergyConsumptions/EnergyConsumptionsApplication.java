32d1c2e8f4701c4555fdc32899901397dda9109f
koval <koval@DESKTOP-TF3DHC9>
2021-12-30 08:27:35 +0200

    Creation and configuration the project

---
+++ EnergyConsumptions/src/main/java/com/kovalchuk/EnergyConsumptions/EnergyConsumptionsApplication.java
@@ -0,0 +1,13 @@
+package com.kovalchuk.EnergyConsumptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyConsumptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyConsumptionsApplication.class, args);
+	}
+
+}
---
+++ EnergyConsumptions/src/test/java/com/kovalchuk/EnergyConsumptions/EnergyConsumptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kovalchuk.EnergyConsumptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyConsumptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
